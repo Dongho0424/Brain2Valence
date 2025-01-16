f996ec779cb714ecd2d2ff7a98807dd92ea0acce
Dongho <pridesam@snu.ac.kr>
2025-01-16 07:49:09 +0000

    Add scripts

---
+++ scripts/brain_only/brain_only.sh
@@ -0,0 +1,32 @@
+device=1
+model_type=brain_only # fixed after this time
+subj=1
+fusion_ver=1 # fixed
+mlp_ver=mlp3 # AdaptiveMaxPool1d(h)
+cat_loss=softmargin
+wd=0.01
+pool_num=2024
+pretrained_wgt_path=./pretrained_wgts/EMOTIC_pretrained_img_extractor_weight/best_model.pth
+project="verify_v1"
+
+for lr in 1e-5 3e-5 5e-5 1e-4 3e-4 5e-4 8e-6 
+do
+    model_name="brain_only_lr_${lr}"
+    group="brain_only"
+    note="250116_1"
+
+    CUDA_VISIBLE_DEVICES=${device} python3 -W "ignore" main.py --exec_mode train --subj ${subj} \
+    --model-name $model_name --notes $note --group $group --wandb-project $project --wandb-entity "donghochoi" \
+    --epochs 50 --batch-size 52 --lr ${lr} --weight-decay ${wd} --optimizer adamw --scheduler cosine --criterion emotic_SL1 --cat-criterion ${cat_loss} \
+    --task-type brain --pretrained None --image-backbone resnet18 --model-type ${model_type} \
+    --brain-backbone ${mlp_ver} --pool-num ${pool_num} --data roi --cat-only --fusion-ver ${fusion_ver} \
+    --wandb-log 
+
+    CUDA_VISIBLE_DEVICES=${device} python3 -W "ignore" main.py --exec_mode predict --subj ${subj} \
+     --model-name $model_name --notes $note --group $group --wandb-project $project --wandb-entity "donghochoi" \
+    --epochs 50 --batch-size 52 --lr ${lr} --weight-decay ${wd} --optimizer adamw --scheduler cosine --criterion emotic_SL1 --cat-criterion ${cat_loss} \
+    --task-type brain --pretrained None --image-backbone resnet18 --model-type ${model_type} \
+    --brain-backbone ${mlp_ver} --pool-num ${pool_num} --data roi --cat-only --fusion-ver ${fusion_ver} \
+    --best --wandb-log 
+done
+
---
+++ scripts/img_only/EMOTIC_not_frz.sh
@@ -0,0 +1,26 @@
+device=0
+model_type=BI # fixed after this time
+subj=1
+cat_loss=softmargin
+wd=0.01
+pretrained_wgt_path=./pretrained_wgts/EMOTIC_pretrained_img_extractor_weight/best_model.pth
+project="verify_v1"
+
+for lr in 1e-5 3e-5 5e-5 1e-4 3e-4 5e-4 8e-6 
+do
+    model_name="img_roi_EMOTIC_not_frz_lr_${lr}"
+    group="img_roi_EMOTIC_not_frz"
+    note="250116_1"
+
+    CUDA_VISIBLE_DEVICES=${device} python3 main.py --exec_mode train --subj ${subj} \
+    --model-name $model_name --notes $note --group $group --wandb-project $project --wandb-entity "donghochoi" \
+    --epochs 50 --batch-size 52 --lr ${lr} --weight-decay $wd --optimizer adamw --scheduler cosine --criterion emotic_SL1 --cat-criterion ${cat_loss} \
+    --task-type emotic --pretrained EMOTIC --image-backbone resnet18 --model-type ${model_type} --data brain3d --with-nsd --cat-only \
+    --wandb-log --wgt-path ${pretrained_wgt_path}
+
+    CUDA_VISIBLE_DEVICES=${device} python3 main.py --exec_mode predict --subj ${subj} \
+    --model-name $model_name --notes $note --group $group --wandb-project $project --wandb-entity "donghochoi" \
+    --epochs 50 --batch-size 52 --lr ${lr} --weight-decay $wd --optimizer adamw --scheduler cosine --criterion emotic_SL1 --cat-criterion ${cat_loss} \
+    --task-type emotic --pretrained EMOTIC --image-backbone resnet18 --model-type ${model_type} --data brain3d --with-nsd --cat-only \
+    --best --wandb-log --wgt-path ${pretrained_wgt_path}
+done
